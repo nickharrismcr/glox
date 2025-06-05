ab594ef4eb3ada927d2df45447afc9e9e4d4e80a
Nick Harris <nicholas.harris@leboat.com>
2025-06-05 14:15:38 +0100

    float array struct get,set,clear

--- lox/obj_builtin_farray.go
+++ lox/obj_builtin_farray.go
@@ -7,6 +7,26 @@ type FloatArray struct {
 	data          []float64
 }
 
+func (f *FloatArray) get(x, y int) float64 {
+	if x < 0 || x >= f.width || y < 0 || y >= f.height {
+		panic(fmt.Sprintf("Index out of bounds: (%d, %d) for array size %dx%d", x, y, f.width, f.height))
+	}
+	return f.data[y*f.width+x]
+}
+
+func (f *FloatArray) set(x, y int, value float64) {
+	if x < 0 || x >= f.width || y < 0 || y >= f.height {
+		panic(fmt.Sprintf("Index out of bounds: (%d, %d) for array size %dx%d", x, y, f.width, f.height))
+	}
+	f.data[y*f.width+x] = value
+}
+
+func (f *FloatArray) clear(value float64) {
+	for i := range f.data {
+		f.data[i] = value
+	}
+}
+
 type FloatArrayObject struct {
 	BuiltInObject
 	value *FloatArray
@@ -24,7 +44,7 @@ func (o *FloatArrayObject) String() string {
 	return fmt.Sprintf("<FloatArray %dx%d>", o.value.width, o.value.height)
 }
 
-func (o *FloatArrayObject) Type() ObjectType {
+func (o *FloatArrayObject) getType() ObjectType {
 	return OBJECT_FLOAT_ARRAY
 }
 
@@ -37,7 +57,7 @@ func (o *FloatArrayObject) GetMethod(name string) *BuiltInObject {
 				yval := vm.stack[arg_stackptr+1]
 				x := xval.asInt()
 				y := yval.asInt()
-				return makeFloatValue(o.value.data[y*o.value.width+x], false)
+				return makeFloatValue(o.value.get(x, y), false)
 			},
 		}
 	case "set":
@@ -49,7 +69,16 @@ func (o *FloatArrayObject) GetMethod(name string) *BuiltInObject {
 				x := xval.asInt()
 				y := yval.asInt()
 				f := fval.asFloat()
-				o.value.data[y*o.value.width+x] = f
+				o.value.set(x, y, f)
+				return makeNilValue()
+			},
+		}
+	case "clear":
+		return &BuiltInObject{
+			function: func(argCount int, arg_stackptr int, vm *VM) Value {
+				fval := vm.stack[arg_stackptr]
+				f := fval.asFloat()
+				o.value.clear(f)
 				return makeNilValue()
 			},
 		}
